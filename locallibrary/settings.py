0dca843895cf008f32004047476d9c132cb8b122
Tharlada-Uday <udayram9652@gmail.com>
2026-06-01 21:38:12 +0530

    Updated the settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mytestsite.urls'
+ROOT_URLCONF = 'locallibrary.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mytestsite.wsgi.application'
+WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 
 # Database
